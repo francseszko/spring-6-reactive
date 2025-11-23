c44c486095b149406077ff715d10d0f761ef68a7
francseszko <francseszko@gmail.com>
2025-11-23 15:23:01 +0100

    add mapstruct

---
+++ src/main/java/guru/springframework/spring6reactive/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6reactive.mappers;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import guru.springframework.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    Beer beerDTOToBeer(BeerDTO beerDTO);
+
+    BeerDTO beerToBeerDTO(Beer beer);
+}
