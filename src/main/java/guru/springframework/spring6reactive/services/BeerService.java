1b92d6600a5ec90eaabb6faab2c4322d4820956e
francseszko <francseszko@gmail.com>
2025-11-25 17:44:07 +0100

    implementing patch existing beer

--- src/main/java/guru/springframework/spring6reactive/services/BeerService.java
+++ src/main/java/guru/springframework/spring6reactive/services/BeerService.java
@@ -14,4 +14,6 @@ public interface BeerService {
     Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO);
 
     Mono<BeerDTO>  updateExistingBeer(Integer beerId, BeerDTO beerDTO);
+
+    Mono<BeerDTO>  patchExistingBeer(Integer beerId, BeerDTO beerDTO);
 }
--- src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
@@ -5,6 +5,7 @@ import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.repositoriers.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -44,6 +45,30 @@ public class BeerServiceImpl implements BeerService {
                     foundBeer.setPrice(beerDTO.getPrice());
                     foundBeer.setUpc(beerDTO.getUpc());
                     foundBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+                    return foundBeer;
+                }).flatMap(beerRepository::save)
+                .map(beerMapper::beerToBeerDTO);
+    }
+
+    @Override
+    public Mono<BeerDTO> patchExistingBeer(Integer beerId, BeerDTO beerDTO) {
+        return beerRepository.findById(beerId)
+                .map(foundBeer -> {
+                    if (StringUtils.hasText(beerDTO.getBeerName())) {
+                        foundBeer.setBeerName(beerDTO.getBeerName());
+                    }
+                    if (StringUtils.hasText(beerDTO.getBeerStyle())) {
+                        foundBeer.setBeerStyle(beerDTO.getBeerStyle());
+                    }
+                    if (beerDTO.getPrice() != null) {
+                        foundBeer.setPrice(beerDTO.getPrice());
+                    }
+                    if (StringUtils.hasText(beerDTO.getUpc())) {
+                        foundBeer.setUpc(beerDTO.getUpc());
+                    }
+                    if (beerDTO.getQuantityOnHand() != null) {
+                        foundBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+                    }
                     return foundBeer;
                 }).flatMap(beerRepository::save)
                 .map(beerMapper::beerToBeerDTO);
